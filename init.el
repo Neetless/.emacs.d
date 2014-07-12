b6456362ae5b57784263d8d1e64b087f9da87a9a
Toshiro Ogita <toshi@Metisi.local>
2014-07-13 02:30:36 +0900

    add rotate, indent setting, recentf setting, whitespace setting and new js2-mode

--- init.el
+++ init.el
@@ -3,8 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; load-path & package initialization
 (setq load-path
-      (append '("~/.emacs.d/elisp")
-	      load-path))
+      (append '("~/.emacs.d/elisp") load-path))
 (add-to-list 'load-path' "~/.emacs.d/elpa")
 
 (require 'package)
@@ -52,7 +51,18 @@
 ;;color-theme
 (load-theme 'zen-and-art t)
 
+;; tab setting
+(setq num-for-tab-setting 2)
+(setq-default tab-width num-for-tab-setting)
+(setq default-tab-width num-for-tab-setting)
+(setq tab-stop-list())
+(while (<= num-for-tab-setting 256)
+  (setq tab-stop-list `(,@tab-stop-list ,num-for-tab-setting))
+  (setq num-for-tab-setting (+ num-for-tab-setting default-tab-width)))
 
+(setq-default indent-tabs-mode nil)
+
+(global-set-key "\C-m" 'newline-and-indent)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; setting for mac
@@ -62,12 +72,9 @@
 (set-frame-font "fontset-15")
 
 ;; change japanese font
-(set-fontset-font nil 'japanese-jisx0208
-		  (font-spec :family "Hiragino Kaku Gothic ProN"))
+(set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Hiragino Kaku Gothic ProN"))
 (setq face-font-rescale-alist
       '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2)))
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; setting for windows
@@ -124,4 +131,49 @@
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
-		    
+
+;; js2-mode
+;;(require 'js2-mode)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; js2 indent setting
+;; http://witterk.blogspot.jp/2012/04/emacs-no-tab.html
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 2)
+ '(tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; rotate.el
+(require 'rotate)
+(global-set-key (kbd "C-t") 'rotate-window)
+
+;; recentf mode
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; whiitespace setting
+(setq whitespace-style
+      '(tabs tab-mark spaces space-mark))
+(setq whitespace-space-regexp "\\(\x3000+\\)")
+(setq whitespace-display-mappings
+      '((space-mark ?\x3000 [?\□])
+        (tab-mark   ?\t   [?\xBB ?\t])
+        ))
+(require 'whitespace)
+(global-whitespace-mode 1)
+(set-face-foreground 'whitespace-space "LightSlateGray")
+(set-face-background 'whitespace-space "DarkSlateGray")
+(set-face-foreground 'whitespace-tab "LightSlateGray")
+(set-face-background 'whitespace-tab "DarkSlateGray")
