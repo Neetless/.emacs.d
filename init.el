47633b6a6d9dd6d1439150cfce9eedec2901d3f4
Toshiro Ogita <toshi@Metisi.local>
2015-01-04 16:59:45 +0900

    20150104 current mac configuration

--- init.el
+++ init.el
@@ -8,9 +8,9 @@
 
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")
-			 ("org" . "http://orgmode.org/elpa/")))
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
 ;; japanese environment 
@@ -49,7 +49,7 @@
 (setq command-line-default-directory "~/")
 
 ;;color-theme
-(load-theme 'zen-and-art t)
+(load-theme 'misterioso t)
 
 ;; tab setting
 (setq num-for-tab-setting 2)
@@ -62,7 +62,51 @@
 
 (setq-default indent-tabs-mode nil)
 
-(global-set-key "\C-m" 'newline-and-indent)
+;;(global-set-key "\C-m" 'newline-and-indent)
+
+;; emacs-client (need .zshrc setting also)
+(server-start)
+
+;; python tab setting
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (setq indent-tabs-mode nil)
+             (setq indent-level 2)
+             (setq python-indent 2)
+             (setq tab-width 2)))
+
+;; jedi
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)  
+
+;; flymake
+(require 'flymake)
+(require 'flymake-cursor)
+
+;; pep8
+;; /usr/local/bin/pep8check.sh
+(when (load "flymake" t) 
+  (defun flymake-pyflakes-init () 
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                       'flymake-create-temp-inplace)) 
+           (local-file (file-relative-name 
+                        temp-file 
+                        (file-name-directory buffer-file-name)))) 
+      (list "/usr/local/bin/pep8check.sh" (list local-file)))) 
+  
+  (add-to-list 'flymake-allowed-file-name-masks 
+               '("\\.py\\'" flymake-pyflakes-init))) 
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(load-library "flymake-cursor")
+(global-set-key [f10] 'flymake-goto-prev-error)
+(global-set-key [f11] 'flymake-goto-next-error)
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+(exec-path-from-shell-copy-env "PYTHONPATH")
+(exec-path-from-shell-copy-env "PATH")
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; setting for mac
@@ -75,6 +119,18 @@
 (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Hiragino Kaku Gothic ProN"))
 (setq face-font-rescale-alist
       '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2)))
+
+;; clipboard share 
+
+;; (defun copy-from-osx ()
+;;  (shell-command-to-string "pbpaste"))
+;; (defun paste-to-osx (text &optional push)
+;;  (let ((process-connection-type nil))
+;;      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;        (process-send-string proc text)
+;;        (process-send-eof proc))))
+;;(setq interprogram-cut-function 'paste-to-osx)
+;;(setq interprogram-paste-function 'copy-from-osx)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; setting for windows
@@ -122,6 +178,14 @@
 
 ;; org-mode
 (require 'org)
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "WAIT(w)" "|" "STARTED(s)" "CANCELED(c)" "DONE(d)")))
+(setq org-todo-keyword-faces
+      '(("WAIT" . (:foreground "yellow" :weight bold))
+        ("STARTED" . (:foreground "red" :weight bold))
+        ("CANCELED" . (:foreground "green" :weight bold))))
+
+
 
 ;; org confluence export 
 (require 'ox-confluence)
@@ -177,3 +241,14 @@
 (set-face-background 'whitespace-space "DarkSlateGray")
 (set-face-foreground 'whitespace-tab "LightSlateGray")
 (set-face-background 'whitespace-tab "DarkSlateGray")
+
+;; taskjuggler-mode
+(require 'taskjuggler-mode)
+
+;; split window
+(split-window-right)
+(other-window 1)
+(split-window-below)
+(other-window 1)
+(other-window 1)
+(recentf-open-files)
